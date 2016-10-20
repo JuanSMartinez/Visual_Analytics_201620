7765c3ce4039523785420717123d6aa98671e84b
JuanSMartinez <js.martinez777@uniandes.edu.co>
2016-10-20 09:27:48 -0500

    controles radiales

--- Visualizacion/HorizonChart/js/app.js
+++ Visualizacion/HorizonChart/js/app.js
@@ -17,7 +17,18 @@
     var scaleYears = d3.scaleLinear().range([0, width]);
     
     //Round sliders
-    function slider(circumference_r, scale, data){
+    function slider(circumference_r, array){
+        //Remove duplicates
+        var newArray = [];
+        array.forEach(function(d){
+            var index = newArray.indexOf(d)
+            if(index < 0 )
+                newArray.push(d);
+        });
+        
+        var delta = 2*Math.PI/(newArray.length);
+        var angularRange = d3.range(0, 2*Math.PI, delta);
+        
         var drag = d3.drag()
               .on("start", dragstarted)
               .on("drag", dragged)
@@ -43,28 +54,62 @@
           .attr("cy", function(d) { return d.y;})
           .call(drag);
         
-        
         var txt = container.append("g")
                 .selectAll('labels')
-                .data(data).enter().append("text")
-                .attr("x", function(d) { return scale[d]; })
-                .attr("y", function(d) { return scale[d]; })
-                .text( function (d) { return "(" +scale[d]+")"; })
+                .data(angularRange).enter().append("text")
+                .attr("style", function(d){return anchor(d);})
+                .attr("x", function(d) { return getX(d); })
+                .attr("y", function(d) { return getY(d); })
+                .text( function (d) { return newArray[angularRange.indexOf(d)]; })
                 .attr("font-family", "sans-serif")
                 .attr("font-size", "10px")
-                .attr("fill", "black");
-
+                .attr("fill", "black")
+                .attr("transform", function(d){return rotate(d);});
         
-        function getX(value, radius){
-            d_from_origin = Math.sqrt(Math.pow(d3.event.x,2)+Math.pow(d3.event.y,2));
-            alpha = Math.acos(d3.event.x/d_from_origin);
-            return radius*Math.cos(alpha);
+        var line = container.append("g")
+                .selectAll('lines')
+                .data(angularRange).enter().append("line")
+                .attr("x1", function(d) { return getX1(d); })
+                .attr("y1", function(d) { return getY1(d); })
+                .attr("x2", function(d) { return getX(d); })
+                .attr("y2", function(d) { return getY(d); })
+                .style("stroke-width", 1)
+                .style("stroke", "black")
+                .style("fill", "none");
+        
+        function anchor(alpha){
+            var cos = Math.round(Math.cos(alpha) * 1000) / 1000;
+            if(Math.sign(cos) >= 0)
+                return "text-anchor: start";
+            else if(Math.sign(cos) < 0)
+                return "text-anchor: end";
         }
         
-        function getY(value, radius){
-            d_from_origin = Math.sqrt(Math.pow(d3.event.x,2)+Math.pow(d3.event.y,2));
-            alpha = Math.acos(d3.event.x/d_from_origin);
-            return d3.event.y < 0 ? -radius*Math.sin(alpha) : radius*Math.sin(alpha);
+        function rotate(alpha){
+            var cos = Math.round(Math.cos(alpha) * 1000) / 1000;
+            var sgncos = Math.sign(cos);
+            var angle = sgncos >= 0 ? alpha*180/Math.PI : sgncos*(180 - alpha*180/Math.PI);
+            return "rotate(" + angle +","+getX(alpha)+","+getY(alpha)+")";
+        }
+        
+        function getX1(alpha){
+            var cos = Math.round(Math.cos(alpha) * 1000) / 1000;
+            return circumference_r*cos
+        }
+        
+        function getY1(alpha){
+            var sin = Math.round(Math.sin(alpha) * 1000) / 1000;
+            return circumference_r*sin
+        }
+        
+        function getX(alpha){
+            var cos = Math.round(Math.cos(alpha) * 1000) / 1000;
+            return circumference_r*cos + Math.sign(cos)*8;
+        }
+        
+        function getY(alpha){
+            var sin = Math.round(Math.sin(alpha) * 1000) / 1000;
+            return circumference_r*sin + Math.sign(sin)*8;
         }
 
         function dragstarted(d) {
@@ -92,16 +137,8 @@
 
     
     function update(data){
-        
-        scaleMinutes.domain(d3.extent(data, function(d){return d["Minuto"];}));
-        svg.append("g")
-            .attr("transform", "translate("+margin.left+"," + (effHeight - 100)+ ")")
-            .call(d3.axisBottom(scaleMinutes));
-        
-        //Create sliders for months, days, hours, minutes and seconds
-        var radius = [150, 120, 90, 60, 30];
-        console.log(scaleMinutes);
-        radius.forEach(function(r){slider(r, scaleMinutes, data.map(function(d){return d["Minuto"]}))});
+        var radius = [150, 120, 90];
+        radius.forEach(function(r){slider(r, data.map(function(d){return d["Minuto"];}))});
      
     };
     
