555118dbf7c4673d134a55187abedc8f6564270a
JuanSMartinez <js.martinez777@uniandes.edu.co>
2016-11-28 22:19:54 -0500

    ETL

--- ETL/main.py
+++ ETL/main.py
@@ -3,20 +3,43 @@ import zipfile
 import xml.etree.ElementTree as etree
 from StringIO import StringIO
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
+#import matplotlib.pyplot as plt
+#import seaborn as sns
 import buses
 import json
 import glob
+import os
+import sys
+import fnmatch
+import re
 
 base_path = 'Data/'
-pcontrol = '*/*/*/*/*/e-track-pcontrol-*.kmz'
+pcontrol = 'Data/*/*/*/*/e-track-pcontrol-*.kmz'
 
 
 def main():
-    global  pcontrol
-    for name in glob.glob(pcontrol):
-        print name
+    global pcontrol
+    matches = []
+    for root, dirnames, filenames in os.walk('Data'):
+        for filename in fnmatch.filter(filenames, 'e-track-pcontrol-*.kmz'):
+            matches.append(os.path.join(root, filename))
+
+    records = np.array([])
+    i = 0
+    for f in matches:
+        i += 1
+        print "Procesando archivo", i, "de", len(matches)
+        records_kmz = read_kmz(f)
+        formatted = format_speed_records(records_kmz)
+        records = np.concatenate((records,formatted.tolist()))
+
+    print 'Recuperados', len(records),"archivos utiles"
+    print 'Generando JSON ...'
+    final = open("buses_final.json", "wb")
+    json.dump(records.tolist(), final)
+    final.close()
+
+
     '''
     records = read_kmz('pcontrol.kmz')
     formatted = format_speed_records(records)
@@ -31,18 +54,22 @@ def format_speed_records(records):
     for r in records:
         entry = dict()
         #TODO: generacion aleatoria de lineas hasta poderlas encontrar en los datos
-        entry['Linea'] = buses.get_bus()
-        entry['Dia'] = int(r["Fecha"].split("/")[0])
-        entry['Mes'] = int(r["Fecha"].split("/")[1])
-        entry['Anio'] = int(r["Fecha"].split("/")[2])
-        #entry['Fecha'] = r["Fecha"]
-        entry['Hora'] = int(r["Hora"].split(":")[0])
-        entry['Minuto'] = int(r["Hora"].split(":")[1])
-        entry['Segundo'] = int(r["Hora"].split(":")[2])
-        entry['Velocidad'] = float(r["Velocidad"].rstrip("Km/h"))
-        entry['Latitud'] = float(r["Lat"])
-        entry['Longitud'] = float(r["Lon"])
-        formatted_records = np.append(formatted_records, entry)
+        #entry['Linea'] = buses.get_bus()
+        try:
+            entry['Linea'] = str(re.search(": ([0-9]+)", r['Recorrido']).group(1))
+            entry['Dia'] = int(r["Fecha"].split("/")[0])
+            entry['Mes'] = int(r["Fecha"].split("/")[1])
+            entry['Anio'] = int(r["Fecha"].split("/")[2])
+            #entry['Fecha'] = r["Fecha"]
+            entry['Hora'] = int(r["Hora"].split(":")[0])
+            entry['Minuto'] = int(r["Hora"].split(":")[1])
+            entry['Segundo'] = int(r["Hora"].split(":")[2])
+            entry['Velocidad'] = float(r["Velocidad"].rstrip("Km/h"))
+            entry['Latitud'] = float(r["Lat"])
+            entry['Longitud'] = float(r["Lon"])
+            formatted_records = np.append(formatted_records, entry)
+        except AttributeError:
+            pass
 
     return formatted_records
 
